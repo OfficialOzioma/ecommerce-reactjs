3e9d07f1c6232efbbc6ea89b5bfd4a2f37a8b11e
officialozioma <dikep15@gmail.com>
2026-02-27 13:04:32 +0100

    fixed issues with coderabbit

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -58,8 +58,10 @@ export default function CartProvider({ children }) {
 
 export function useCart() {
     const context = useContext(CartContext);
+
     if (!context) {
         throw new Error("useCart must be used within a CartProvider");
     }
+
     return context;
 }
